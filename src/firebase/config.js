8f60a9edd5cc751a1a694575f4b2ff1a5ed560ec
Michellas93 <100272452+Michellas93@users.noreply.github.com>
2023-06-15 17:32:33 +0200

    firebase v9 compat change

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,5 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import firebase from "firebase/compat/app";
+import "firebase//compat/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHNduHpRpdHAh5ut7aMRD6Rx_fCT-kico",
